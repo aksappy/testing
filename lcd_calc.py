1976fe52e9f984671fc9509ca6a2d05460d9b0f4
Nishithasreekumar <nishithasreekumar@gmail.com>
2025-11-14 23:07:04 +0530

    calc

--- lcd_calc.py
+++ lcd_calc.py
@@ -2,8 +2,8 @@ from lcd_inp import get_fractions
 import math
 c=get_fractions()
 deno = [d for (_, d) in c]
-print("List of fractions=",c)
-print("List of denominators",deno)
+print("Fractions=",c)
+print("Denominators=",deno)
 lcd = deno[0]
 for d in deno[1:]:
     lcd = lcd * d // math.gcd(lcd, d)
